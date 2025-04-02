6ecc76112adbe15086047eb4adebf5d3b7fcd4b3
mutkarsh06 <mutkarsh701@gmail.com>
2025-04-02 11:46:35 +0530

    Project Day 5

---
+++ frontend/src/components/View.jsx
@@ -0,0 +1,35 @@
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+const View = () => {
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        viewdata();
+    },[])
+    const viewdata = async ()=>{
+        const res=await axios.get('http://localhost:9000/users');
+        console.log(res);
+        setUsers(res.data);
+    }
+
+  return (
+    <div>
+        <table style={{border: '2px solid red',backgroundColor:'greenyellow',width:'100%'}}>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+            </tr>
+      {
+        users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.age}</p>
+          </div>
+        ))
+      }
+      </table>
+    </div>
+  )
+}
+
+export default View
