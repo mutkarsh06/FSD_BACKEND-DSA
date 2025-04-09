107ee62cbac85116386dbdab6b7bd6b75331bb4d
mutkarsh06 <mutkarsh701@gmail.com>
2025-04-09 11:19:12 +0530

    render update

--- frontend/src/components/Register1.jsx
+++ frontend/src/components/Register1.jsx
@@ -7,7 +7,7 @@ const Register = () => {
             name: e.target.name.value,
             age:e.target.age.value
         }
-        await axios.post('http://localhost:9000/users',user)
+        await axios.post('https://fsd-backendd-dsa.onrender.com/users',user)
         alert('User registered Successfullly.')
     }
     return (
--- frontend/src/components/Update.jsx
+++ frontend/src/components/Update.jsx
@@ -7,7 +7,7 @@ const Update = () => {
         const name = e.target.name.value
         const age = e.target.age.value
         const data={name,age}
-        await axios.put(`http://localhost:9000/users/${id}`,data)
+        await axios.put(`https://fsd-backendd-dsa.onrender.com/users/${id}`,data)
         alert("Success")
     }
 
