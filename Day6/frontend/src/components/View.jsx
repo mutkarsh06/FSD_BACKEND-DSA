84a658a30e9412608a864b38ca1bdcf3559edac3
mutkarsh06 <mutkarsh701@gmail.com>
2025-04-09 12:16:59 +0530

    New Changes

--- Day6/frontend/src/components/View.jsx
+++ Day6/frontend/src/components/View.jsx
@@ -13,9 +13,9 @@ const View = () => {
   return (
     <div>
       <h1>Registered Users list</h1>
-      <table style={{border:'1px solid black',backgroundColor:'plum',width:'100%',textAlign:'center',borderCollapse: 'collapse'}}>
+      <table style={{border:'1px solid black',backgroundColor:'lightorange',width:'100%',textAlign:'center',borderCollapse: 'collapse'}}>
         <thead >
-          <tr style={{border:'3px solid black',color:'white'}}>
+          <tr style={{border:'3px solid black',color:'pink'}}>
           <th>ID</th>
           <th>Name</th>
           <th>Age</th>
